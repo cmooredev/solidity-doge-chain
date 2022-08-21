ddd83f213277398d466cb7e1932d8cfa6aa48588
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-08-21 14:37:34 -0700

    first commit

---
+++ dogechain.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+contract HelloDogechain {
+
+    string message;
+    address public lastMessenger;
+    address owner;
+
+    mapping(address => uint) helloPlace;
+
+    constructor() {
+        owner = msg.sender;
+        message = "Hello Dogechain!";
+    }
+
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+
+    receive() external payable {
+        // React to receiving ether
+        require(msg.value > 0.01 ether, "You need to send at least 0.01");
+        lastMessenger = msg.sender;
+        helloPlace[msg.sender] += msg.value;
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function withdrawMoney() public {
+        require(msg.sender == owner);
+        address payable to = payable(msg.sender);
+        to.transfer(getBalance());
+    }
+
+    function setHelloFrens(string memory newHello) public payable {
+        require(helloPlace[msg.sender] != 0);
+        require(msg.sender == lastMessenger);
+        message = newHello;
+    }
+
+    function splitPot() public payable {
+        uint totalPot = address(this).balance;
+        require(totalPot > 100 * (10**18));
+        address payable firstHalf = payable(lastMessenger);
+        firstHalf.transfer(getBalance()/2);
+        address payable secondHalf = payable(lastMessenger);
+        secondHalf.transfer(getBalance()/2);
+        address payable to = payable(lastMessenger);
+        to.transfer(getBalance());
+    }
+
+}
